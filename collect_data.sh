9fdf0dede551f872a3c218eecb613ffa4e6abfd7
matt-amplelabs <43194171+matt-amplelabs@users.noreply.github.com>
2018-10-10 14:07:48 -0400

    added ref to meal skill

--- collect_data.sh
+++ collect_data.sh
@@ -4,4 +4,5 @@ curl https://www.toronto.ca/app_content/homeless-help-respitesites/ | python -m 
 
 curl https://www.toronto.ca/app_content/homeless-help-shelters/ | python -m json.tool > homeless-help-shelters.json
 
+# Ref: https://www.toronto.ca/community-people/housing-shelter/homeless-help/#meals
 curl https://www.toronto.ca/app_content/homeless-help-meals/ | python -m json.tool > homeless-help-meals.json
